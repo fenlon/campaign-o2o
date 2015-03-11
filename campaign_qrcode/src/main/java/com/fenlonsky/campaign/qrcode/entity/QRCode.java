3ae67950ba577b054a6fc01f0222c93ec10c77db
Fenlon <Fenlon@Fenlon-PC.lan>
2015-03-11 23:19:11 +0800

    添加注释

--- campaign_qrcode/src/main/java/com/fenlonsky/campaign/qrcode/entity/QRCode.java
+++ campaign_qrcode/src/main/java/com/fenlonsky/campaign/qrcode/entity/QRCode.java
@@ -8,12 +8,17 @@ import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 public class QRCode implements Serializable {
 	
 	private static final long serialVersionUID = 6425927132341801365L;
-	/****/
+	/** 编码内容 **/
 	private String content;
+	/** 二维码容错等级 **/
 	private ErrorCorrectionLevel correctionLevel = ErrorCorrectionLevel.L;
+	/** 大小 **/
 	private Integer size = QRCodeUtil.DEFAULT_HEIGHT;
+	/** 前景色 **/
 	private Integer foregroundColor = QRCodeUtil.DEFAULT_FORE_GROUND_COLOR;
+	/** 背景色 **/
 	private Integer backGroundColor = QRCodeUtil.DEFAULT_BACK_GROUND_COLOR;
+	/** logo URL **/
 	private String logoUrl;
 	
 	public String getContent() {
