d0b40e6cd4c04187d7310d306a34529178d6149b
fenlon <fenlonxiong@gmail.com>
2015-03-13 14:50:24 +0800

    初始化base模块

---
+++ campaign-base/src/main/java/com/fenlonsky/campaign/base/App.java
@@ -0,0 +1,13 @@
+package com.fenlonsky.campaign.base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
