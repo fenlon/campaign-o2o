92c8f661dce877ff2a83fea916dba161bd6c6659
Fenlon <Fenlon@Fenlon-PC.lan>
2015-03-15 23:53:17 +0800

    完善 管理模块

---
+++ campaign-admin/src/main/webapp/WEB-INF/pages/admin/main/index.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>管理首页</title>
+
+<script type="text/javascript">
+	var ctx = '${pageContext.request.contextPath	}';
+	var extjsPath = "http://222.22.91.35:8080/ext-4.2.1.883/";
+</script>
+
+<link
+	href="${pageContext.request.contextPath }/resources/style/common/bootstrap3/bootstrap.min.css"
+	rel="stylesheet" type="text/css" />
+<script type="text/javascript"
+	src="${pageContext.request.contextPath }/resources/js/angularjs/angular.min.js"></script>
+<script type="text/javascript"
+	src="${pageContext.request.contextPath }/resources/js/qrcode/index.js"></script>
+</style>
+</head>
+<body>
+	<div class="container">
+		<h1>this is admin module</h1>
+	</div>
+</body>
+</html>
---
+++ campaign-admin/src/main/webapp/WEB-INF/resources/js/qrcode/index.js
@@ -0,0 +1,57 @@
+var indexModule = angular.module("indexModule", []);
+
+indexModule.controller("indexController", [
+		'$scope',
+		'$http',
+		function($scope, $http) {
+
+			$scope.show = false;
+			$scope.fore = "FF6600";
+			$scope.back = "FFFFCC";
+			$scope.codeUrl = ctx + "/qrcode/get.html?content=''";
+
+			$scope.qrcode = {
+				content : "",
+				errorLevel : 'H',
+				size : 300,
+				foregroundColor : 0,
+				backgroundColor : 0,
+				logoUrl : null
+			};
+
+			var baseUrl = ctx + "/qrcode/get.html?content=";
+			$scope.generateCode = function(content) {
+				if (content == "") {
+					$scope.show = false;
+					alert("请先输入要编码的内容!");
+					return;
+				}
+
+				$scope.qrcode.foregroundColor = parseInt($scope.fore, 16);
+				$scope.qrcode.backgroundColor = parseInt($scope.back, 16);
+
+				$scope.show = true;
+				$scope.codeUrl = encodeURI(baseUrl + content + "&errorLevel="
+						+ $scope.qrcode.errorLevel + "&size="
+						+ $scope.qrcode.size + "&foregroundColor="
+						+ $scope.qrcode.foregroundColor + "&backgroundColor="
+						+ $scope.qrcode.backgroundColor + "&logoUrl="
+						+ $scope.qrcode.logoUrl);
+			};
+
+			$scope.downloadCode = function() {
+				$scope.generateCode($scope.qrcode.content);
+				var url = ctx + "/qrcode/d_load.html";
+				// $scope.qrcode.errorLevel = 2;
+
+				$http.get(url, {
+					params : $scope.qrcode,
+
+				}).success(function(data, status, headers, config) {
+					// 加载成功之后做一些事
+				}).error(function(data, status, headers, config) {
+					// 处理错误
+				});
+			}
+
+		} ]);
