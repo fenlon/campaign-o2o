f9fce611d6dfa0c922f1bcf3e5d56e96f457bc6b
Fenlon <Fenlon@Fenlon-PC>
2015-05-10 16:00:16 +0800

    添加门店业务逻辑类

---
+++ campaign-admin/src/main/java/com/fenlonsky/campaign/admin/service/impl/StoreManagerImpl.java
@@ -0,0 +1,20 @@
+package com.fenlonsky.campaign.admin.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.fenlonsky.campaign.admin.bean.Store;
+import com.fenlonsky.campaign.admin.dao.StoreDao;
+import com.fenlonsky.campaign.admin.service.StoreManager;
+import com.fenlonsky.campaign.base.service.impl.GenericManagerImpl;
+
+@Service
+public class StoreManagerImpl extends GenericManagerImpl<Store, Long> implements StoreManager {
+	
+	StoreDao storeDao;
+	
+	public void setStoreDao(StoreDao storeDao) {
+		this.storeDao = storeDao;
+		this.dao = this.storeDao;
+	}
+	
+}
