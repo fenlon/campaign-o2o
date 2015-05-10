8e90765c4702342ebd67e35c66e5034976073968
Fenlon <Fenlon@Fenlon-PC>
2015-05-10 15:56:13 +0800

    update name

---
+++ campaign-admin/src/main/java/com/fenlonsky/campaign/admin/service/AccountManager.java
@@ -0,0 +1,7 @@
+package com.fenlonsky.campaign.admin.service;
+
+import com.fenlonsky.campaign.admin.bean.Account;
+import com.fenlonsky.campaign.base.service.GenericManager;
+
+public interface AccountManager extends GenericManager<Account, Long> {
+}
