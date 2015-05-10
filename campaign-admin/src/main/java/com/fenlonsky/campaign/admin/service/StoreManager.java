1fd45f73470637e9d823ba5e456f6c4f9f45d12c
Fenlon <Fenlon@Fenlon-PC>
2015-05-10 16:02:58 +0800

    添加门店业务逻辑类

---
+++ campaign-admin/src/main/java/com/fenlonsky/campaign/admin/service/StoreManager.java
@@ -0,0 +1,7 @@
+package com.fenlonsky.campaign.admin.service;
+
+import com.fenlonsky.campaign.admin.bean.Store;
+import com.fenlonsky.campaign.base.service.GenericManager;
+
+public interface StoreManager extends GenericManager<Store, Long> {
+}
